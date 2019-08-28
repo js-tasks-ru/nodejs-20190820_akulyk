228e5c076a7dc4d48487c5211d03d0c3f5255a0e
Alexander Kulyk <akulyk@yandex.ua>
2019-08-28 23:36:11 +0300

    2-module home tasks finish

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -2,14 +2,38 @@ const stream = require('stream');
 const os = require('os');
 
 class LineSplitStream extends stream.Transform {
+  #data = [];
+
   constructor(options) {
     super(options);
   }
 
   _transform(chunk, encoding, callback) {
+    let data = chunk.toString();
+    data = data.trim();
+
+    if (data.indexOf(os.EOL) !== -1) {
+      let tmp = data.split(os.EOL);
+
+      for (let i = 0; i < tmp.length - 1; i++) {
+
+        this.#data.push(tmp[i]);
+        this.push(this.#data.join(''));
+        this.#data = [];
+      }
+
+      this.#data.push(tmp.pop());
+      callback(null);
+      return;
+
+    } else {
+      this.#data.push(data);
+    }
+    callback(null);
   }
 
   _flush(callback) {
+    callback(null, this.#data.join(''));
   }
 }
 
