9a4a7f4cd03e1cea030b88512207b9c67d1ec471
Alexander Kulyk <akulyk@yandex.ua>
2019-09-06 17:14:19 +0300

    5 module 2 task complete

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -1,17 +1,43 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(require('koa-static')('public'));
+app.use(require('koa-static')(__dirname + '/public'));
 app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
 
+
+let messageReceived;
+
+let promise = createPromise();
+
 router.get('/subscribe', async (ctx, next) => {
+  const message = await promise;
+  if (!message) {
+    ctx.statusCode = 405;
+    ctx.body = null;
+  } else {
+    ctx.statusCode = 200;
+    ctx.body = message;
+  }
+  promise = createPromise();
+  return;
 });
 
 router.post('/publish', async (ctx, next) => {
+  const message = ctx.request.body.message;
+  if (message) {
+    messageReceived(message);
+  }
+
+  ctx.statusCode = 200;
+  ctx.body = 'Ok';
 });
+
+function createPromise() {
+  return new Promise((resolve) => messageReceived = resolve);
+}
 
 app.use(router.routes());
 
