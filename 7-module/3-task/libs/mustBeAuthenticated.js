a670f1ae3a7022f63ccc665e24d30f8fdc468248
Alexander Kulyk <akulyk@yandex.ua>
2019-09-12 22:53:20 +0300

    7-module 3-task complete

--- 7-module/3-task/libs/mustBeAuthenticated.js
+++ 7-module/3-task/libs/mustBeAuthenticated.js
@@ -1,3 +1,17 @@
-module.exports = function mustBeAuthenticated(ctx, next) {
-  return next();
+class AuthError extends Error {
+  constructor(message = 'Пользователь не залогинен') {
+    super(message);
+    this.status = 401;
+  }
+}
+
+module.exports = {
+  AuthError,
+  mustBeAuthenticated: function mustBeAuthenticated(ctx, next) {
+    if (!ctx.user) {
+      throw new AuthError();
+    }
+    return next();
+  },
 };
+
